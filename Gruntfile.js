eee8a915b6b57e721b4e8e98e3bd448798f7aefc
Oliver Brook <olibrook@gmail.com>
2015-11-05 10:47:23 -0800

    Run server and watch LESS sources in the serve task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,11 +37,14 @@ module.exports = function (grunt) {
         options: {
           port: 8000,
           base: 'public',
-          keepalive: true,
           debug: true
         }
       }
     }
-
   });
+
+    grunt.registerTask('serve', [
+      'connect:server',
+      'watch'
+    ]);
 };
